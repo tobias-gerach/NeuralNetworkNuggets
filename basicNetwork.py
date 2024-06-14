f5a9bd39cb4b7be0a29b8075ec37d7f628e02ecb
tobias-gerach <tgerach@web.de>
2024-06-14 16:11:46 +0200

    first version of the network. Does not really work well when training on the entire data set

--- basicNetwork.py
+++ basicNetwork.py
@@ -1,13 +1,28 @@
+import random
+import time
+
 import numpy as np
-import pandas as pd
 
 from data.MNIST.preprocess import load_data
+
 
 def ReLU(z: any) -> any:
     return np.maximum(0, z)
 
+
 def dReLU(z: any) -> any:
     return z > 0
+
+
+def Sigmoid(z):
+    """The sigmoid function."""
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+def dSigmoid(z):
+    """Derivative of the sigmoid function."""
+    return Sigmoid(z) * (1 - Sigmoid(z))
+
 
 class Network(object):
     """
@@ -30,16 +45,16 @@ class Network(object):
         """
         Returns the output of the network after a single pass
 
-        :param a: input 
+        :param a: input
         """
         for b, w in zip(self.biases, self.weights):
-            a = ReLU(np.dot(w,a) + b)
+            # a = ReLU(np.dot(w, a) + b)
+            a = Sigmoid(np.dot(w, a) + b)
             return a
-        
+
     def backpropagate(self, x, y):
         """
-        Return a tuple of weights dCdw and biases dCdb for each layer.
-        dCdw and dCdb 
+        Return the cost gradient w.r.t. weights dCdw and biases dCdb
 
         :param x: input data/features
         :param y: labels
@@ -52,32 +67,69 @@ class Network(object):
 
         # Set activation for input layer as x
         a = x
+        a_list.append(a)
 
         # Feedforward: for each layer l = 2,3,...,L compute weighted inputs z[l] and activations a[l]
         for b, w in zip(self.biases, self.weights):
-            z_list.append(np.dot(w,a) + b)
-            a = ReLU(z_list[-1])
+            z_list.append(np.dot(w, a) + b)
+            # a = ReLU(z_list[-1])
+            a = Sigmoid(z_list[-1])
             a_list.append(a)
 
         # Compute the output error and gradient
-        delta = (a_list[-1] - y) * dReLU(z_list[-1])
-        dCdw[-1] = np.dot(delta, a_list[-2].transpose)
+        # delta = (a_list[-1] - y) * dReLU(z_list[-1])
+        delta = (a_list[-1] - y) * dSigmoid(z_list[-1])
+        dCdw[-1] = np.dot(delta, a_list[-2].transpose())
         dCdb[-1] = delta
 
         # Backpropagate error: for each layer l = L-1,L-2,...,2 compute error delta
-        for layer in range(2,self.num_layers):
-            delta = np.dot(self.weights[-layer+1].transpose, delta) * dReLU(z_list[-layer])
-            dCdw[-layer] = np.dot(delta, a_list[-layer-1].transpose)
+        for layer in range(2, self.num_layers):
+            # delta = np.dot(self.weights[-layer + 1].transpose(), delta) * dReLU(z_list[-layer])
+            delta = np.dot(self.weights[-layer + 1].transpose(), delta) * dSigmoid(
+                z_list[-layer]
+            )
+            dCdw[-layer] = np.dot(delta, a_list[-layer - 1].transpose())
             dCdb[-layer] = delta
 
         return dCdw, dCdb
 
+    def gradientDescent(self, training_data, epochs, learning_rate, test_data=None):
+        for epoch in range(epochs):
+            time1 = time.time()
+            random.shuffle(training_data)
+            dCdw = [np.zeros(w.shape) for w in self.weights]
+            dCdb = [np.zeros(b.shape) for b in self.biases]
+            for x, y in training_data:
+                delta_dCdw, delta_dCdb = self.backpropagate(x, y)
+                dCdw = [nw + dnw for nw, dnw in zip(dCdw, delta_dCdw)]
+                dCdb = [nb + dnb for nb, dnb in zip(dCdb, delta_dCdb)]
+            self.weights = [w - learning_rate * nw for w, nw in zip(self.weights, dCdw)]
+            self.biases = [b - learning_rate * nb for b, nb in zip(self.biases, dCdb)]
+            time2 = time.time()
+            print("Epoch {0} complete in {1:.2f} seconds".format(epoch, time2 - time1))
+
+        if test_data:
+            n = len(test_data)
+            print(
+                "Accuracy after {0} epochs: {1} / {2}".format(
+                    epoch, self.evaluate(test_data), n
+                )
+            )
+
+    def evaluate(self, test_data) -> int:
+        """
+        Return the number of test inputs for which the neural
+        network outputs the correct result.
+        """
+        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
 
 
 def main():
     tr_d, va_d, te_d = load_data()
 
-
+    net = Network([784, 16, 10])
+    net.gradientDescent(tr_d, 10, 3.0, te_d)
 
 
 if __name__ == "__main__":
--- data/MNIST/preprocess.py
+++ data/MNIST/preprocess.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 
 
-def load_data() -> tuple[any, any, any]:
+def load_data():
     """
     Returns the MNIST data containing the training data, the validation data, and the test data.
     The training data consists of 50,000 entries, while the validation and test data consists of 10,000 entries each.
@@ -14,6 +14,8 @@ def load_data() -> tuple[any, any, any]:
     containing the input image.  ``y`` is a 10-dimensional
     numpy.ndarray representing the unit vector corresponding to the
     correct digit for ``x``.
+    In case of the validation and test data, ``x`` is a 784-dimensional array containing the input data
+    and ``y`` is the corresponding classification.
     """
 
     f = gzip.open("./data/MNIST/mnist.pkl.gz", "rb")
@@ -21,6 +23,13 @@ def load_data() -> tuple[any, any, any]:
     u.encoding = "latin1"
     training_data, validation_data, test_data = u.load()
     f.close()
+    training_inputs = [np.reshape(x, (784, 1)) for x in training_data[0]]
+    training_results = [one_encode_vector(y) for y in training_data[1]]
+    training_data = list(zip(training_inputs, training_results))
+    validation_inputs = [np.reshape(x, (784, 1)) for x in validation_data[0]]
+    validation_data = list(zip(validation_inputs, validation_data[1]))
+    test_inputs = [np.reshape(x, (784, 1)) for x in test_data[0]]
+    test_data = list(zip(test_inputs, test_data[1]))
     return (training_data, validation_data, test_data)
 
 def one_encode_vector(j: int):
